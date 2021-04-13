141f280cef357fca0a67df8d343347265ac32f1c
Sergey Muhlinin <sealmu@gmail.com>
2021-04-13 12:26:44 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 const withGraphql = require("next-graphql-loader");
 
 module.exports = withGraphql({});
-
-
-// module.exports = {  
-//     webpack(config) {
-//       config.resolve.modules.push(__dirname)
-//       return config;
-//     },
-// }
--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,161 +1,105 @@
-
-// import { mergeTypeDefs } from '@graphql-tools/merge';
-
-// import { makeExecutableSchema, mergeSchemas } from 'graphql-tools';
-
-
-//import firebase from '../../../utils/firebase';
-
 import Cors from 'micro-cors';
-import { ApolloServer, gql }  from 'apollo-server-micro'; // 
+import { ApolloServer, gql } from 'apollo-server-micro'; // from 'apollo-server-cloud-functions'; // 
 
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
+type Todo = {
+    id: number;
+    description: string;
+    done: boolean;
+};
 
-import todoResolvers from '../../../schema/resolvers/todo.resolvers';
-import todoTypes from '../../../schema/types/todo.types.graphql';
+const todos: Todo[] = [
+    {
+        id: 1,
+        description: 'todo - 1',
+        done: false,
+    },
+];
 
-// type Todo = {
-//     id: number;
-//     description: string;
-//     done: boolean;
-// };
+const todoResolvers = {
+    Query: {
+        todos: (): Todo[] => {
+            return todos;
+        },
+        todo: (_parent: never, { id }: { id: string | number }): Todo => {
+            return todos.find((t) => t.id == id);
+        },
+    },
 
-// const todos: Todo[] = [
-//     {
-//         id: 1,
-//         description: 'todo - 1',
-//         done: false,
-//     },
-// ];
+    Mutation: {
+        createTodo: async (
+            _: never,
+            { description, done }: { description: string; done: boolean }
+        ):
+        Promise<Todo> => {
+            const todo: Todo = {
+                id: new Date().getTime(),
+                description,
+                done,
+            };
 
-// const todoResolvers = {
-//     Query: {
-//         todos: (): Todo[] => {
-//             return todos;
-//         },
-//         todo: (_parent: never, { id }: { id: string | number }): Todo => {
-//             return todos.find((t) => t.id == id);
-//         },
-//     },
+            todos.push(todo);
+            return todo;
+        },
+        completeTodo: async (_: never, { id }: { id: string | number }): Promise<Todo> => {
+            const todo = todos.find((t) => t.id == id);
 
-//     Mutation: {
-//         createTodo: async (
-//             _: never,
-//             { description, done }: { description: string; done: boolean }
-//         ):
-//         Promise<Todo> => {
-//             const todo: Todo = {
-//                 id: new Date().getTime(),
-//                 description,
-//                 done,
-//             };
+            if (todo) {
+                todo.done = true;
+            }
 
-//             todos.push(todo);
-//             return todo;
-//         },
-//         completeTodo: async (_: never, { id }: { id: string | number }): Promise<Todo> => {
-//             const todo = todos.find((t) => t.id == id);
+            return todo;
+        },
+    },
+};
 
-//             if (todo) {
-//                 todo.done = true;
-//             }
+const todoTypes = gql`
+    type Todo {
+        id: ID!
+        description: String!
+        done: Boolean!
+    }
 
-//             return todo;
-//         },
-//     },
-// };
+    type Query {
+        todos: [Todo]!
+        todo(id: ID!): Todo
+    }
 
-// const todoTypes = gql`
-//     type Todo {
-//         id: ID!
-//         description: String!
-//         done: Boolean!
-//     }
+    type Mutation {
+        createTodo(description: String!, done: Boolean): Todo
+        completeTodo(id: ID!): Todo
+    }
+`;
 
-//     type Query {
-//         todos: [Todo]!
-//         todo(id: ID!): Todo
-//     }
-
-//     type Mutation {
-//         createTodo(description: String!, done: Boolean): Todo
-//         completeTodo(id: ID!): Todo
-//     }
-// `;
-
-
-// firebase();
 
 
 
 const cors = Cors({
-    // allowMethods: ['POST', 'OPTIONS'],
     origin: "*",
     credentials: true,
 });
 
-
-// type CustomSocket = Exclude<NextApiResponse<any>['socket'], null> & {
-//     server: Parameters<ApolloServer['installSubscriptionHandlers']>[0] & {
-//         apolloServer?: ApolloServer;
-//         apolloServerHandler?: any;
-//     };
-// };
-
-// type CustomNextApiResponse<T = any> = NextApiResponse<T> & {
-//     socket: CustomSocket;
-// };
-
 const getApolloServerHandler = async ({ res }: { res?: NextApiResponse }): Promise<any> => {
-    //if (globalThis.schema) return globalThis.schema;
 
     if (res) {
         console.log('res init');
     }
 
-    // const typeDefs = mergeTypeDefs([todoTypes]);
-    // const resolvers = [todoResolvers];
-    // const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-    // const schemas = mergeSchemas({ schemas: [schema].filter(Boolean) });
-
-
     const apolloServer = new ApolloServer({
-        // schema: schemas,
         typeDefs: [todoTypes],
 	    resolvers: [todoResolvers],
         introspection: true,
-        // playground: {
-        //     settings: {
-        //         'request.credentials': 'same-origin',
-        //         //"schema.polling.enable": false
-        //     },
-        // },
         playground: true,
-        // {
-        //     subscriptionEndpoint: '/api/graphiql',
-
-        //     // settings: {
-        //     //     'request.credentials': 'same-origin',
-        //     // },
-        // },
-        // formatError: formatError,
         context: ({ req, res }) => {
             if (!req || !res) console.log('no');
             return { };
         },
     });
 
-    const handler = apolloServer.createHandler({ path: '/api/graphql' }); // path: '/api/graphql'
-
-    //const corsHandler = cors(handler);
-    const corsHandler = cors((req, res) =>
-        req.method === "OPTIONS" ? res.end() : handler(req, res)
-    );
-
-    //globalThis.schema = corsHandler;
+    const handler = apolloServer.createHandler({  path: '/api/graphql' });
+    const corsHandler = cors(handler);
     return corsHandler;
 };
 
